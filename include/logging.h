ed2577cfd0f49c6db0c6dbb7e6ccbdd910866fc4
ThePedroo <pedroolimpioguerra@gmail.com>
2026-01-07 18:08:28 -0300

    add: CSOLoader logs support for non-Android Linux systems
    This commit adds support for fprintf-based logging in CSOLoader to be utilized in desktop Linux systems.

--- include/logging.h
+++ include/logging.h
@@ -9,33 +9,51 @@
 
 #include <stdio.h>
 
-#include <android/log.h>
+#define LOG_TAG "CSOLoader"
 
-#define LOG_TAG "csoloader"
+#ifdef __ANDROID__
+  #include <android/log.h>
 
-#ifdef CSOLOADER_DEBUG
-  #ifdef __ANDROID__
-    #define LOGD(...) __android_log_print(ANDROID_LOG_DEBUG, LOG_TAG, __VA_ARGS__)
-    #define LOGI(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
-    #define LOGW(...) __android_log_print(ANDROID_LOG_WARN, LOG_TAG, __VA_ARGS__)
-    #define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__)
-    #define LOGF(...) do { __android_log_print(ANDROID_LOG_FATAL, LOG_TAG, __VA_ARGS__); abort(); } while(0)
-    #define PLOGE(fmt, ...) __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, fmt ": %s", ##__VA_ARGS__, strerror(errno))
+  #ifdef CSOLOADER_DEBUG
+    #ifdef __ANDROID__
+      #define LOGD(...) __android_log_print(ANDROID_LOG_DEBUG, LOG_TAG, __VA_ARGS__)
+      #define LOGI(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
+      #define LOGW(...) __android_log_print(ANDROID_LOG_WARN, LOG_TAG, __VA_ARGS__)
+      #define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__)
+      #define LOGF(...) do { __android_log_print(ANDROID_LOG_FATAL, LOG_TAG, __VA_ARGS__); abort(); } while(0)
+      #define PLOGE(fmt, ...) __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, fmt ": %s", ##__VA_ARGS__, strerror(errno))
+    #else
+      #define LOGD(...) fprintf(stderr, "[D] " LOG_TAG ": " __VA_ARGS__); fprintf(stderr, "\n")
+      #define LOGI(...) fprintf(stderr, "[I] " LOG_TAG ": " __VA_ARGS__); fprintf(stderr, "\n")
+      #define LOGW(...) fprintf(stderr, "[W] " LOG_TAG ": " __VA_ARGS__); fprintf(stderr, "\n")
+      #define LOGE(...) fprintf(stderr, "[E] " LOG_TAG ": " __VA_ARGS__); fprintf(stderr, "\n")
+      #define LOGF(...) do { fprintf(stderr, "[F] " LOG_TAG ": " __VA_ARGS__); fprintf(stderr, "\n"); abort(); } while(0)
+      #define PLOGE(fmt, ...) fprintf(stderr, "[E] " LOG_TAG ": " fmt ": %s\n", ##__VA_ARGS__, strerror(errno))
+    #endif
   #else
+    #define LOGD(...) do {} while(0)
+    #define LOGI(...) do {} while(0)
+    #define LOGW(...) do {} while(0)
+    #define LOGE(...) do {} while(0)
+    #define LOGF(...) do { abort(); } while(0)
+    #define PLOGE(fmt, ...) do {} while(0)
+  #endif
+#else
+  #ifdef CSOLOADER_DEBUG
     #define LOGD(...) fprintf(stderr, "[D] " LOG_TAG ": " __VA_ARGS__); fprintf(stderr, "\n")
     #define LOGI(...) fprintf(stderr, "[I] " LOG_TAG ": " __VA_ARGS__); fprintf(stderr, "\n")
     #define LOGW(...) fprintf(stderr, "[W] " LOG_TAG ": " __VA_ARGS__); fprintf(stderr, "\n")
     #define LOGE(...) fprintf(stderr, "[E] " LOG_TAG ": " __VA_ARGS__); fprintf(stderr, "\n")
     #define LOGF(...) do { fprintf(stderr, "[F] " LOG_TAG ": " __VA_ARGS__); fprintf(stderr, "\n"); abort(); } while(0)
     #define PLOGE(fmt, ...) fprintf(stderr, "[E] " LOG_TAG ": " fmt ": %s\n", ##__VA_ARGS__, strerror(errno))
+  #else
+    #define LOGD(...) do {} while(0)
+    #define LOGI(...) do {} while(0)
+    #define LOGW(...) do {} while(0)
+    #define LOGE(...) do {} while(0)
+    #define LOGF(...) do { abort(); } while(0)
+    #define PLOGE(fmt, ...) do {} while(0)
   #endif
-#else
-  #define LOGD(...) do {} while(0)
-  #define LOGI(...) do {} while(0)
-  #define LOGW(...) do {} while(0)
-  #define LOGE(...) do {} while(0)
-  #define LOGF(...) do { abort(); } while(0)
-  #define PLOGE(fmt, ...) do {} while(0)
 #endif
 
 #endif /* LOGGING_H */
